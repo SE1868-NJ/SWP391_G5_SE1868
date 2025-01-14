746e27ffdcf92a9ed6a581b9d7ae6344b75ab207
anhduc111 <154323330+anhduc111@users.noreply.github.com>
2025-01-14 19:17:56 +0700

    duc 14/01/2025

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import "./App.css";
 
-import Dashboard from "./router/Dashboard";
+import Dashboard from "./router/Dashboard/Dashboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
